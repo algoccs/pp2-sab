8b1183c6f815809ee9b67e834953bee46791d3f2
algoccs <algodemoacc@gmail.com>
2026-05-31 11:00:04 -0400

    Refactor database.py for improved structure and clarity

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ DB = "quises.sqlite"
 conn = None
 cursor = None
 
+
 def open():
     global conn, cursor
     conn = sqlite3.connect(DB)
@@ -11,29 +12,32 @@ def open():
     # parametros de config de la db
     cursor.execute('PRAGMA foreign_keys=on')
 
+
 def close():
     cursor.close()
     conn.close()
+
 
 def execute_query(query):
     cursor.execute(query)
     conn.commit()
 
+
 def create_tables():
     tables = [
-    '''CREATE TABLE IF NOT EXISTS quiz (
+        '''CREATE TABLE IF NOT EXISTS quiz (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL);''',
-  
-    '''CREATE TABLE IF NOT EXISTS question (
+
+        '''CREATE TABLE IF NOT EXISTS question (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     question_name TEXT NOT NULL,
     correct VARCHAR(100) NOT NULL,
     wrong_1 VARCHAR(10) NOT NULL,
     wrong_2 VARCHAR(10) NOT NULL,
     wrong_3 VARCHAR(10) NOT NULL);''',
-    
-    '''CREATE TABLE IF NOT EXISTS quiz_content (
+
+        '''CREATE TABLE IF NOT EXISTS quiz_content (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     quiz_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
@@ -60,14 +64,21 @@ def add_quises():
     close()
     print('Se ingresaron los datos de la tabla quiz!')
 
+
 def add_questions():
     questions = [
-        ('¿Cuántos meses en un año tienen 28 días?', 'Todos', 'Uno', 'Ninguno','Dos'),
-        ('¿Qué aspecto tendrá el acantilado verde si se cae en el Mar Rojo?', 'Mojado', 'Rojo', 'No cambiará', 'Púrpura'),
-        ('¿Con qué mano es mejor mezclar el té?', 'Con una cuchara', 'Derecha', 'Izquierda', 'Cualquiera'),
-        ('¿Qué no tiene longitud, profundidad, ancho, o altura pero puede medirse?', 'Tiempo', 'Estupidez', 'El mar','Aire'),
-        ('¿Cuándo es posible sacar agua con una red?', 'Cuando el agua está congelada', 'Cuando no hay peces', 'Cuando los peces de colores nadan lejos', 'Cuando la red se rompe'),
-        ('¿Qué es más grande que un elefante y no pesa nada?', 'La sombra de un elefante','Un globo','Un paracaídas', 'Una nube')
+        ('¿Cuántos meses en un año tienen 28 días?',
+         'Todos', 'Uno', 'Ninguno', 'Dos'),
+        ('¿Qué aspecto tendrá el acantilado verde si se cae en el Mar Rojo?',
+         'Mojado', 'Rojo', 'No cambiará', 'Púrpura'),
+        ('¿Con qué mano es mejor mezclar el té?',
+         'Con una cuchara', 'Derecha', 'Izquierda', 'Cualquiera'),
+        ('¿Qué no tiene longitud, profundidad, ancho, o altura pero puede medirse?',
+         'Tiempo', 'Estupidez', 'El mar', 'Aire'),
+        ('¿Cuándo es posible sacar agua con una red?', 'Cuando el agua está congelada',
+         'Cuando no hay peces', 'Cuando los peces de colores nadan lejos', 'Cuando la red se rompe'),
+        ('¿Qué es más grande que un elefante y no pesa nada?',
+         'La sombra de un elefante', 'Un globo', 'Un paracaídas', 'Una nube')
     ]
     open()
     cursor.executemany('''INSERT INTO question
@@ -78,22 +89,24 @@ def add_questions():
     print('Se ingresaron los datos de la tabla!')
 
 
-def add_links(): # ESTRUCTURAR CUESTIONARIOS
+def add_links():  # ESTRUCTURAR CUESTIONARIOS
     links = []
 
     link = input('Desea ingresar un enlace? (y/n): ')
     while link.lower() == 'y':
-        quiz_id = int(input('ID del quiz: ')) 
-        question_id = int(input('ID de la pregunta: ')) 
+        quiz_id = int(input('ID del quiz: '))
+        question_id = int(input('ID de la pregunta: '))
 
         links.append((quiz_id, question_id))
         link = input('Desea ingresar otro? (y/n): ')
 
     if links:
         open()
-        cursor.executemany('INSERT INTO quiz_content (quiz_id, question_id) VALUES (?, ?);', links)
+        cursor.executemany(
+            'INSERT INTO quiz_content (quiz_id, question_id) VALUES (?, ?);', links)
         conn.commit()
         close()
+
 
 def destroy_db():
     tables = ['quiz_content', 'quiz', 'question']
@@ -102,7 +115,8 @@ def destroy_db():
         execute_query(f'DROP TABLE IF EXISTS {table};')
     close()
     print('😂')
-        
+
+
 def show_tables():
     tables = ['quiz', 'question', 'quiz_content']
     open()
@@ -123,6 +137,7 @@ def show_tables():
             print('Error:', error)
     close()
 
+
 def get_next_question(question_id=0, quiz_id=1):
     open()
 
@@ -140,24 +155,12 @@ def get_next_question(question_id=0, quiz_id=1):
         AND quiz_content.quiz_id = ?
         ORDER BY quiz_content.id
         LIMIT 1'''
-    
+
     cursor.execute(query, [question_id, quiz_id])
     result = cursor.fetchone()
     close()
     return result
 
-
-def show(table):
-    query = 'SELECT * FROM ' + table
-    open()
-    cursor.execute(query)
-    print(cursor.fetchall())
-    close()
-
-def show_tables():
-    show('question')
-    show('quiz')
-    show('quiz_content')
 
 def get_quises():
     open()
@@ -167,12 +170,19 @@ def get_quises():
     return result
 
 
-def main(): 
+# def show(table):
+#     query = 'SELECT * FROM ' + table
+#     open()
+#     cursor.execute(query)
+#     print(cursor.fetchall())
+#     close()
+
+def main():
     show_tables()
     add_links()
 
     print(get_next_question(2, 1))
-    
+
 
 if __name__ == "__main__":
     main()
